803ae465f8acc20a641fde65af59fac28ccbb763
Melanie <mho-hoffmann@web.de>
2024-10-08 09:55:33 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ function generatePoem(event) {
   let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+
   axios.get(apiURL).then(displayPoem);
 }
 
